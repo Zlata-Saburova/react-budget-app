2081f76f372c7da18c7edf691e48fa3e9cbdc093
Zlata <zlatasaburova97@gmail.com>
2022-04-25 22:57:46 +0300

    feat(container): add media points

--- src/components/Container/ContainerStyle.ts
+++ src/components/Container/ContainerStyle.ts
@@ -8,4 +8,12 @@ export const Container = styled.div`
   width: 100%;
   margin: 0 auto;
   padding: 0 20px;
+
+  @media (min-width: 768px) {
+    max-width: 500px;
+  }
+
+  @media (min-width: 1024px) {
+    max-width: 700px;
+  }
 `;
