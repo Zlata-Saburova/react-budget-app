7acbd2a34ffc576d0e0ad67b916aef274765ab75
Zlata <zlatasaburova97@gmail.com>
2022-04-25 16:20:58 +0300

    feat(Container): add component Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 function App() {
-  return (
-    
-  );
+  return StyledApp;
 }
+
+const StyledApp = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default App;
---
+++ src/components/Container/ContainerStyle.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  gap: 30px;
+
+  min-width: 320px;
+  width: 100%;
+  margin: 0 auto;
+`;
