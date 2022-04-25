c5c41fbfcfe352a0c4e2687eee113c59cc6ffba9
Zlata <zlatasaburova97@gmail.com>
2022-04-25 16:45:07 +0300

    feat(Title): add component Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import styled from "styled-components";
+import { Container } from "./components/Container/ContainerStyle";
+import { Title } from "./components/Title/Title";
 
 function App() {
-  return StyledApp;
+  return (
+    <StyledApp>
+      <Container>
+        <Title>Budget App</Title>
+      </Container>
+    </StyledApp>
+  );
 }
 
 const StyledApp = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-top: 30px;
 `;
 
 export default App;
---
+++ src/components/Title/Title.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  text-align: left;
+
+  font-size: 24px;
+  line-height: 29px;
+  font-weight: 700;
+  color: rgb(0, 0, 0);
+`;
