4b48698f038309297813c76bed138a2a92732d9a
Zlata <zlatasaburova97@gmail.com>
2022-04-26 14:45:23 +0300

    fix(container): change media point

--- src/components/Container/StyledContainer.ts
+++ src/components/Container/StyledContainer.ts
@@ -4,16 +4,12 @@ export const Container = styled.div`
   display: grid;
   gap: 30px;
 
-  min-width: 320px;
+  max-width: 320px;
   width: 100%;
   margin: 0 auto;
   padding: 0 20px;
 
   @media (min-width: 768px) {
     max-width: 500px;
-  }
-
-  @media (min-width: 1024px) {
-    max-width: 700px;
   }
 `;
