1ecb359f70d165a59804afaf403e4f60ce96d49b
Zlata <zlatasaburova97@gmail.com>
2022-04-26 15:09:12 +0300

    refactor(list): change height

--- src/components/List/StyledList.ts
+++ src/components/List/StyledList.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledList = styled.ul`
   min-height: 200px;
-  max-height: 205px;
+  max-height: 210px;
   overflow: auto;
 
   list-style-type: none;
