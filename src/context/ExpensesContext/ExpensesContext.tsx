4987d34897e95fcdcfbde4cf0386edabdd047f9e
Zlata <zlatasaburova97@gmail.com>
2022-04-29 14:47:50 +0300

    docs: istall uuid

--- src/context/ExpensesContext/ExpensesContext.tsx
+++ src/context/ExpensesContext/ExpensesContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, FC, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import {
   IExpenses,
   IExpensesContex,
@@ -14,8 +15,8 @@ const useExpensesContexValue = () => {
   const [expensesContext, setExpensesContext] = useState<IExpensesContex>(
     () => ({
       expenses: [
-        { id: "123", name: "ps5", cost: 5868 },
-        { id: "125", name: "table", cost: 568 },
+        { id: uuidv4(), name: "ps5", cost: 5868 },
+        { id: uuidv4(), name: "table", cost: 568 },
       ],
       setExpenses: (newExpenses: IExpenses[]) => {
         setExpensesContext((ctx) => ({ ...ctx, expenses: newExpenses }));
