c4f210b86626dc6f1b44a27dab175e36e840d20c
Zlata <zlatasaburova97@gmail.com>
2022-04-29 16:45:50 +0300

    feat(listItem): add function to delete buy

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,3 +1,5 @@
+import { useContext, useState } from "react";
+import { ExpensesContext } from "../../context/ExpensesContext/ExpensesContext";
 import { IExpenses } from "../../context/ExpensesContext/types";
 import { Badge } from "../Badge/Badge";
 import { Close } from "../Close/Close";
@@ -8,9 +10,16 @@ interface IProps {
 }
 
 export const ListItem: React.FC<IProps> = ({ buy }) => {
+  const { expenses, setExpenses } = useContext(ExpensesContext);
+
+  const deleteBuy = () => {
+    const newExpenses = expenses.filter((expense) => expense.id !== buy.id);
+
+    setExpenses(newExpenses);
+  };
   return (
     <StyledListItem>
-      {buy.name} <Badge cost={buy.cost} /> <Close />
+      {buy.name} <Badge cost={buy.cost} /> <Close onClick={deleteBuy} />
     </StyledListItem>
   );
 };
