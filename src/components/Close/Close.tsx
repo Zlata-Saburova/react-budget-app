cb8b1e25f418f8b6db67ab1cf117a887cc240a72
Zlata <zlatasaburova97@gmail.com>
2022-04-29 16:46:15 +0300

    feat(close): drill props for delete

--- src/components/Close/Close.tsx
+++ src/components/Close/Close.tsx
@@ -1,6 +1,10 @@
 import { StyledClose } from "./styles";
 import close from "../../assets/icons/close.svg";
 
-export const Close = () => {
-  return <StyledClose src={close} alt="close" />;
+interface IProps {
+  onClick: () => void;
+}
+
+export const Close = ({ onClick }: IProps) => {
+  return <StyledClose src={close} alt="close" onClick={onClick} />;
 };
