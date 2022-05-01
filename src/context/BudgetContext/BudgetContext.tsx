0b09cc5b94a2ae2a537a87917687f32286c881d7
Zlata <zlatasaburova97@gmail.com>
2022-05-01 17:59:27 +0300

    feat(context): add budget context

---
+++ src/context/BudgetContext/BudgetContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+import { IBudgetContext, IBudgetContextProviderProps } from "./types";
+
+const BudgetContext = createContext<IBudgetContext>({
+  budget: 0,
+  setBudget: () => {},
+});
+
+const useBudgetContextValue = () => {
+  const [budgetContext, setBudgetContext] = useState<IBudgetContext>(() => ({
+    budget: 0,
+    setBudget: (newBudget) => {
+      setBudgetContext((ctx) => ({ ...ctx, budget: newBudget }));
+    },
+  }));
+
+  return budgetContext;
+};
+
+export const useBudgetContext = () => useContext(BudgetContext);
+
+export const BudgetContextProvider = ({
+  children,
+}: IBudgetContextProviderProps) => {
+  return (
+    <BudgetContext.Provider value={useBudgetContextValue()}>
+      {children}
+    </BudgetContext.Provider>
+  );
+};
---
+++ src/context/BudgetContext/types.ts
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+export interface IBudgetContextProviderProps {
+  children: ReactNode;
+}
+
+export interface IBudgetContext {
+  budget: number;
+  setBudget: (newBudget: number) => void;
+}
+
+// export interface IBudget {
+//   budget: number;
+// }
