a4e4aa036969f9adc74feb8cca5b32a0c16c7668
Zlata <zlatasaburova97@gmail.com>
2022-05-07 20:38:46 +0300

    feat(app): remove logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,12 @@
 import styled from "styled-components";
-import { Card } from "./components/Card/Card";
 import { Container } from "./components/Container/StyledContainer";
-import { EditButton } from "./components/EditButton/EditButton";
 import { CustomSelect } from "./components/CustomSelect/Select";
-import { SearchInput } from "./components/SearchInput/SearchInput";
-import { List } from "./components/List/List";
 import { Form } from "./components/Form/Form";
 import { Title } from "./components/Title/Title";
-import { ChangeEvent, useEffect, useState } from "react";
-import { CardInput } from "./components/CardInput/CardInput";
-import { useBudgetContext } from "./context/BudgetContext/BudgetContext";
-import { SaveButton } from "./components/SaveButton/SaveButton";
-import { useExpensesContext } from "./context/ExpensesContext/ExpensesContext";
-import { useCurrencyContex } from "./context/CurrencyContext/CurrencyContext";
-import { IExpenses } from "./context/ExpensesContext/types";
+import { CardContainer } from "./components/CardContainer/CardContainer";
+import { ListContainer } from "./components/ListContainer/ListContainer";
 
 const App = () => {
-  const [isEdit, setIsEdit] = useState<boolean>(false);
-
-  const handleEditButton = () => {
-    setIsEdit(!isEdit);
-  };
-
-  const [inputValue, setInputValue] = useState<number>(0);
-
-  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(+e.target.value);
-  };
-
-  const { budget, setBudget } = useBudgetContext();
-
-  const handleSaveButton = () => {
-    setIsEdit(!isEdit);
-    setBudget(inputValue);
-  };
-
-  const { expenses } = useExpensesContext();
-
-  const [spent, setSpent] = useState<number>(0);
-
-  const [remaining, setRemaining] = useState<number>(0);
-
-  const [overspent, setOverspent] = useState<number>(0);
-
-  useEffect(() => {
-    const sum = expenses.reduce((acc, buy) => acc + buy.cost, 0);
-    setSpent(sum);
-    setRemaining(budget - sum);
-
-    if (sum > budget) {
-      setOverspent(sum - budget);
-    }
-  }, [budget, expenses]);
-
-  const [type, setType] = useState<string>("remaining");
-
-  useEffect(() => {
-    if (spent > budget) {
-      setType("overspending");
-    } else {
-      setType("remaining");
-    }
-  }, [spent, budget]);
-
-  const [findValue, setFindValue] = useState<string>("");
-  const [findedExpenses, setFindedExpenses] = useState<IExpenses[]>([]);
-
-  const handlerSearch = (e: ChangeEvent<HTMLInputElement>) => {
-    setFindValue(e.target.value);
-  };
-
-  useEffect(() => {
-    const findExpenses = expenses;
-    setFindedExpenses(
-      findExpenses.filter((buy) =>
-        buy.name.toLowerCase().includes(findValue.toLowerCase())
-      )
-    );
-  }, [findValue, expenses]);
-
-  const { currency } = useCurrencyContex();
   return (
     <StyledApp>
       <Container>
@@ -87,33 +14,9 @@ const App = () => {
           <Title>Budget App</Title>
           <CustomSelect />
         </TitleContainer>
-        <Card isEdit type="budget">
-          {isEdit ? (
-            <CardInput handleInput={handleInput} />
-          ) : (
-            `Budget: ${currency}${budget}`
-          )}
-          {isEdit ? (
-            <SaveButton handleSaveButton={handleSaveButton}>Save</SaveButton>
-          ) : (
-            <EditButton handleEditButton={handleEditButton}>Edit</EditButton>
-          )}
-        </Card>
-        <Card type={type}>
-          {type === "remaining"
-            ? `Remaining: ${currency}${remaining}`
-            : `Overspending by ${currency}${overspent}`}
-        </Card>
-        <Card type="spent">
-          Spent so far: {currency}
-          {spent}
-        </Card>
+        <CardContainer />
       </Container>
-      <Container>
-        <Title>Expenses</Title>
-        <SearchInput findValue={findValue} handlerSearch={handlerSearch} />
-        <List findedExpenses={findedExpenses}></List>
-      </Container>
+      <ListContainer />
       <Container>
         <Title>Add Expense</Title>
         <Form></Form>
